6f1f7ddbdff5559f201b838e4c0bc134fcd04566
DiegoAmorimDeSousa <diegoassousa@gmail.com>
2023-02-14 16:41:03 -0300

    Setting file routes and init create roots in css

---
+++ src/global/styles.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background: var(--background-dark);
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  *, button, input, select {
+    border: 0;
+    outline: 0;
+    font-family: 'Archivo', -apple-system, sans-serif;
+  }
+
+  :root {
+    // BACKGROUND
+    --background-dark: #191919;
+
+    // FONT-FAMILY
+    --dm-sans: 'DM Sans';
+
+    // FONT-SIZE
+    --header-size: 20px;
+
+    // FONT-WEIGHT
+    --header-weight: 500;
+
+    // COLOR
+    --light-content: #A7A7A7
+  }
+`;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+function home() {
+  return (
+    <Container>Hello World div</Container>
+  );
+}
+
+export default home;
